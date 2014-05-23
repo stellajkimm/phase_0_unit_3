dc937a314c5c5bd0d163a16cda7b802a4cbcda50
stellajkimm <stellajkimm@gmail.com>
2014-05-23 13:41:08 -0400

    fizzbuzz

--- week_8_and_9/3_Ruby/fizz_buzz/my_solution.rb
+++ week_8_and_9/3_Ruby/fizz_buzz/my_solution.rb
@@ -6,21 +6,30 @@
 # 2. Pseudocode
 
 # define method super_fizzbuzz that takes in an array as a parameter
-# 
+# create a new array called new_array
+# for each element in the array, run through an if statement where
+# if the number % 15 is equal to 0, add "FizzBuzz" into the new_array
+# if the number % 3 is equal to 0, add "Fizz" into the new_array
+# if the number % 5 is equal to 0, add "Buzz" into the new_array
+# else put the number in the new_array
+# return new_array
 
 # 3. Initial Solution
 
 def super_fizzbuzz(array)
 	new_array = []
 	array.each do |num| 
-		case num
-		when num % 15 == 0 then new_array << "FizzBuzz"
-		when num % 3 == 0 then new_array << "Fizz"
-		when num % 5 == 0 then new_array << "Buzz"
-		else new_array << num
+		if num % 15 == 0
+			new_array << "FizzBuzz"
+		elsif num % 3 == 0
+			new_array << "Fizz"
+		elsif num % 5 == 0
+			new_array << "Buzz"
+		else 
+			new_array << num
 		end
 	end
-	puts new_array
+	new_array
 end
 
 
@@ -38,7 +47,7 @@ puts "returns 'Fizz' for multiples of 3"
 puts super_fizzbuzz([3,6,9]) == ["Fizz", "Fizz", "Fizz"]
 
 puts "returns 'Buzz' for multiples of 5"
-puts super_fizzbuzz([5,10,15]) == ["Buzz", "Buzz", "Buzz"]
+puts super_fizzbuzz([5,10,20]) == ["Buzz", "Buzz", "Buzz"]
 
 puts "returns 'FizzBuzz' for multiples of 15"
 puts super_fizzbuzz([15,30,45]) == ["FizzBuzz", "FizzBuzz", "FizzBuzz"]
@@ -52,3 +61,5 @@ puts super_fizzbuzz([15,5,3,1]) == ['FizzBuzz', 'Buzz', 'Fizz', 1]
 
 
 # 5. Reflection 
+
+# easy challenge. we did something like this in before Phase 0 even started. I love Ruby!
